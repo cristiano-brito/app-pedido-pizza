3df1172c6fb63f424334c9604d7584dcdafd6334
cristiano-brito <cristianocomputacao@hotmail.com>
2022-09-03 06:18:51 -0300

    Correção imports AppImpressao da feature 08

--- src/main/java/br/edu/infnet/model/tests/AppImpressao.java
+++ src/main/java/br/edu/infnet/model/tests/AppImpressao.java
@@ -2,11 +2,6 @@ package br.edu.infnet.model.tests;
 
 import br.edu.infnet.interfaces.IPrinter;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class AppImpressao {
 
     public static void relatorio(String mensagem, IPrinter pizza) {
